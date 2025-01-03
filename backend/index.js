e2bc5f8d220f50c9e5e4c885e2503d4bd5389b44
shobhit024 <shobhitparashar24@gmail.com>
2025-01-03 19:31:33 +0530

    add render deploy url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Update this to your frontend URL
+    origin: "https://formbot-gamo.onrender.com", // Update this to your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     credentials: true, // This allows cookies and other credentials to be sent
   })
